392442ac9b84d6879b31bb1ddbf9c9141c076946
mlsml94 <mlsml94@gmail.com>
2020-08-18 20:44:39 -0400

    profile_link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   resources :answers
   resources :questions
   root to: 'pages#home'
   devise_for :users
   get '/forbidden', to: 'authorizations#forbidden', as: 'forbidden'
+  resources :users, only: [:show]
 
   
   
