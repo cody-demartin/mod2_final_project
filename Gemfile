58dfbf98c9fa6947750b1db9a7a7287c2ed9f9bb
mlsml94 <mlsml94@gmail.com>
2020-08-22 12:02:20 -0400

    oauth-success

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'image_processing', '~> 1.2'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem 'activerecord-session_store'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,19 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+    def google_oauth2
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        if @user.persisted?
+            flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+            sign_in_and_redirect @user, event: :authentication
+        else
+            session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+            redirect_to new_user_registration_url
+        end
+    end
+
+
+    def failure
+        flash[:error] = 'We could not log you in. Please try again.'
+        redirect_to new_user_registration_url
+    end
+end
